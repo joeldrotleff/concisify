801df972bfb3dc04554446c77d9a32c4010d2047
Michael Romanenko <michael@romanenko.kg>
2024-05-22 11:35:42 -0700

    Fix build

--- app/lib/ld-server.ts
+++ app/lib/ld-server.ts
@@ -1,20 +1,22 @@
 import LaunchDarkly from "launchdarkly-node-server-sdk";
 import { Config } from "sst/node/config";
 
+let _client: LaunchDarkly.LDClient | undefined;
+
 async function initialize() {
   const client = LaunchDarkly.init(Config.LD_SDK_KEY);
-  globalThis.LaunchDarklyServerClient = await client.waitForInitialization();
+  _client = await client.waitForInitialization();
 
-  return globalThis.LaunchDarklyServerClient;
+  return _client;
 }
 
 async function getClient() {
-  const ldClient = globalThis.LaunchDarklyServerClient;
-  return ldClient ? ldClient : initialize();
+  return _client ? _client : initialize();
 }
 
-async function getVariation(flagKey, context, defaultValue) {
+async function getVariation(flagKey: any, context: any, defaultValue: any) {
   const ldClient = await getClient();
   return ldClient.variation(flagKey, context, defaultValue);
 }
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getClient, getVariation };
